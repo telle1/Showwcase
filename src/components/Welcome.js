3a135ea527f0bcfa6f9f7f7ef598f639dcc87d71
Tiffany Luu <tiffanyell1@gmail.com>
2020-12-31 15:06:41 -0800

    Welcome page HTML/CSS

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,36 @@
+import React from 'react';
+import './welcome.css'
+// const Content = styled.div`
+//     height: 100vh;
+//     box-sizing: border-box;
+//     background: url( ${img} ) no-repeat center center/ cover;
+//     padding: 200px 50px;
+// `
+
+// const Button = styled.button`
+//     border: none;
+//     border-radius: 5px;
+//     padding: 12px;
+//     color: white;
+//     background-color: #636EF8
+// `
+
+// const TextInput = styled.input`
+//     padding: 10px;
+// `
+
+
 function Welcome(){
     return (
-        <h1>test</h1>
+        <div id="welcome">
+                <div className="welcome-input">
+                    <h1>Welcome to your education showcase!</h1>
+                    <p>Help employers learn more about your background and highlight all the accomplishments
+                        you have achieved. </p>
+                    <input type="text" placeholder="Name"></input>
+                    <button type="submit">Enter</button>
+                </div>
+        </div>
     )
 }
 
