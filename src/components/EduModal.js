5379c508ec7294bfa560fd044f234ee6872b1117
Tiffany Luu <tiffanyell1@gmail.com>
2021-01-01 17:22:23 -0800

    Reorganized home page into components

--- src/components/EduModal.js
+++ src/components/EduModal.js
@@ -23,7 +23,7 @@ Modal.setAppElement('#root');
 
 function EduModal({ setEduList, eduList }) {
   const [modalIsOpen, setIsOpen] = useState(false);
-  const [showResults, setShowResults] = useState(false)
+  const [showResults, setShowResults] = useState(false);
   const [schoolRes, setSchoolRes] = useState([]);
   const [eduInfo, setEduInfo] = useState({
     schoolName: '',
@@ -55,29 +55,22 @@ function EduModal({ setEduList, eduList }) {
   };
 
   const handleEdu = (e) => {
-      e.preventDefault();
-      setEduList([...eduList, eduInfo])
-      localStorage.setItem(
-        'edu-list',
-        JSON.stringify([...eduList, eduInfo])
-      );
-
-
-      closeModal();
-      setEduInfo({
-        schoolName: '',
-        degreeType: '',
-        field: '',
-        startDate: '',
-        endDate: '',      
-      })
-
-  }
+    e.preventDefault();
+    setEduList([...eduList, eduInfo]);
+    localStorage.setItem('edu-list', JSON.stringify([...eduList, eduInfo]));
+    closeModal();
+    setEduInfo({
+      schoolName: '',
+      degreeType: '',
+      field: '',
+      startDate: '',
+      endDate: '',
+    });
+  };
 
   return (
     <React.Fragment>
       <button onClick={openModal}>Add new education</button>
-
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
@@ -92,13 +85,14 @@ function EduModal({ setEduList, eduList }) {
         <form className='edu-form' onSubmit={handleEdu}>
           <div className='input-wrapper school-name'>
             <label htmlFor='school-name'> &nbsp; School Name &nbsp; </label>
-            <input required
+            <input
+              required
               type='text'
               placeholder='UCLA'
               value={eduInfo.schoolName}
               onChange={(e) => {
-                setShowResults(true)
-                setEduInfo({ ...eduInfo, schoolName: e.target.value })
+                setShowResults(true);
+                setEduInfo({ ...eduInfo, schoolName: e.target.value });
               }}
             ></input>
           </div>
@@ -108,8 +102,8 @@ function EduModal({ setEduList, eduList }) {
               {schoolRes.map((school) => (
                 <li
                   onClick={(e) => {
-                    setEduInfo({ ...eduInfo, schoolName: school.name })
-                    setShowResults(false)
+                    setEduInfo({ ...eduInfo, schoolName: school.name });
+                    setShowResults(false);
                   }}
                 >
                   {school.name}
@@ -120,13 +114,14 @@ function EduModal({ setEduList, eduList }) {
 
           <div className='input-wrapper'>
             <label htmlFor='degree-type'> &nbsp; Degree Type &nbsp;</label>
-            <select required
+            <select
+              required
               name='degree-type'
               onChange={(e) =>
                 setEduInfo({ ...eduInfo, degreeType: e.target.value })
               }
             >
-              <option defaultValue> -- Select -- </option>
+              <option value=''> -- Select -- </option>
               <option value='Primary education' name='Primary education'>
                 Primary Education
               </option>
@@ -151,14 +146,6 @@ function EduModal({ setEduList, eduList }) {
             </select>
           </div>
 
-          {/* {eduInfo.degreeType == 'Other' ? 
-          <div>
-            <p>Please specify degree:</p>
-            <input type="text" onChange={ (e) => setEduInfo({ ...eduInfo, degreeType: e.target.value}) }>
-
-            </input>
-            </div> : null} */}
-
           <div className='input-wrapper'>
             <label htmlFor='field'> &nbsp; Field of Study &nbsp;</label>
             <input
@@ -171,12 +158,18 @@ function EduModal({ setEduList, eduList }) {
           </div>
 
           <div className='input-wrapper'>
-              <label className="activities" htmlFor='activites'> &nbsp; Activities &nbsp; </label>
-              <textarea rows="4" placeholder='Club Tennis' onChange={(e) =>
-                  setEduInfo({ ...eduInfo, activities: e.target.value })
-                } ></textarea>                   
+            <label className='activities' htmlFor='activites'>
+              {' '}
+              &nbsp; Activities &nbsp;{' '}
+            </label>
+            <textarea
+              rows='4'
+              placeholder='Club Tennis'
+              onChange={(e) =>
+                setEduInfo({ ...eduInfo, activities: e.target.value })
+              }
+            ></textarea>
           </div>
-
 
           <div className='edu-dates'>
             <div className='input-wrapper'>
@@ -184,7 +177,8 @@ function EduModal({ setEduList, eduList }) {
                 {' '}
                 &nbsp; From &nbsp;
               </label>
-              <input required
+              <input
+                required
                 type='date'
                 onChange={(e) =>
                   setEduInfo({ ...eduInfo, startDate: e.target.value })
@@ -196,7 +190,8 @@ function EduModal({ setEduList, eduList }) {
                 {' '}
                 &nbsp; To &nbsp;
               </label>
-              <input required
+              <input
+                required
                 type='date'
                 onChange={(e) =>
                   setEduInfo({ ...eduInfo, endDate: e.target.value })
@@ -204,7 +199,6 @@ function EduModal({ setEduList, eduList }) {
               ></input>
             </div>
           </div>
-
 
           <button type='submit'>Save</button>
         </form>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { generatePath, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import './home.css';
 import EduModal from './EduModal'
 import moment from 'moment'
@@ -7,7 +7,7 @@ import moment from 'moment'
 
 function Home() {
   const location = useLocation();
-  const [name, setName] = useState("" || location.state.name)
+  const [name, setName] = useState(location.state.name)
   const [eduList, setEduList] = useState([])
 
   //Sort by newest education first
@@ -20,24 +20,12 @@ function Home() {
 
   console.log('eduList', eduList)
 
-  if (location.state.name){
-    localStorage.setItem(
-        'name', location.state.name
-      );
-  }
-
   useEffect(() => {
     const eduListStored = localStorage.getItem('edu-list');
     if (eduListStored) {
       const getEduList = JSON.parse(eduListStored);
       setEduList(getEduList);
     }
-    const nameStored = localStorage.getItem('name');
-    if (nameStored) {
-        const getName = nameStored
-        setName(nameStored)
-    }
-
   }, []);
 
 
@@ -45,20 +33,15 @@ function Home() {
     <section id='home'>
       <div className='education-header'>
         <h1>{name}'s education showcase</h1>
-
         <EduModal setEduList={setEduList} eduList={eduList}></EduModal>
       </div>
 
       <div className='education-wrapper'>
         <div className='side-bar'>
-            <h2>My Education</h2>
-            {eduList.map(eduItem => (
-                    <p>{eduItem.schoolName}</p>
-                ))}
+          <SideBar eduList={eduList}/>
         </div>
         <div className='ed-item'>
-            {eduList.map((eduItem,i) => <EduEntry key={i} eduItem={eduItem} eduList={eduList} setEduList={setEduList}/>
-            )}
+          {eduList.map((eduItem,i) => <EduEntry key={i} eduItem={eduItem} eduList={eduList} setEduList={setEduList}/>)}
         </div>
       </div>
     </section>
@@ -67,6 +50,16 @@ function Home() {
 
 export default Home;
 
+function SideBar({ eduList }){
+  return (
+    <React.Fragment>
+      <h2>My Education</h2>
+      {eduList.map((eduItem, i) => (
+              <p key={i}>{eduItem.schoolName}</p>
+          ))}
+    </React.Fragment>
+  )
+}
 
 function EduEntry({ eduItem, eduList, setEduList }){
 
@@ -78,17 +71,17 @@ function EduEntry({ eduItem, eduList, setEduList }){
 
     return (
     <div className="ed-details">
-
         <div className='close' onClick={deleteEduItem}>
           <i className='fas fa-times'></i>
         </div>
-
         <h2>{eduItem.schoolName}</h2>
         <p>
           {eduItem.degreeType}
           {eduItem.field ? <span> in {eduItem.field}</span> : null}
         </p>
-        <sup> <LocalTime date={eduItem.startDate}/> - <LocalTime date={eduItem.endDate}/> </sup>
+        <sup> 
+          <LocalTime date={eduItem.startDate}/> - <LocalTime date={eduItem.endDate}/>
+        </sup>
         {eduItem.activities ? 
         <p>Activities: {eduItem.activities}</p>    
         : null }
