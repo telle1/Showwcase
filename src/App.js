bb19eaa7b8cb4486ec70ae7cd2c1fe8ac4cbeb79
Tiffany Luu <tiffanyell1@gmail.com>
2021-01-01 17:31:24 -0800

    Fixed file organization

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
-import Home from './components/Home'
-import Welcome from './components/Welcome'
+import Home from './components/Home/Home'
+import Welcome from './components/Welcome/Welcome'
 
 function App() {
   return (
