3d0e7581ae018a05675ec1cf499f27f049f0fb6d
Tiffany Luu <tiffanyell1@gmail.com>
2021-01-04 01:03:00 -0800

    cors fix?

--- src/components/EduModal/EduModal.js
+++ src/components/EduModal/EduModal.js
@@ -54,7 +54,7 @@ function EduModal() {
     setSchoolRes([]);
     try {
       const res = await axios.get(
-        `https://universities.hipolabs.com/search?name=${debouncedSchoolName}`,
+        `http://universities.hipolabs.com/search?name=${debouncedSchoolName}`,
         { cancelToken: source.token }
       );
       setSchoolRes(res.data);
